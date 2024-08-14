af2c9ba0abf1470df5918b79d908eb9fec7e874e
Mahfuz <aamahfuz.pro@gmail.com>
2024-08-14 09:01:28 +0600

    basic initialization done!

---
+++ src/components/ContentSection.tsx
@@ -0,0 +1,9 @@
+const ContentSection = () => {
+  return (
+    <div>
+      <h1>Welcome to , ContentSection Page!</h1>
+    </div>
+  );
+};
+
+export default ContentSection;
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,9 @@
+const ProfileCard = () => {
+  return (
+    <div>
+      <h1>Welcome to , ProfileCard Page!</h1>
+    </div>
+  );
+};
+
+export default ProfileCard;
